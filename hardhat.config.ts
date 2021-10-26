47b8a3a8ca47e76916876ba972223ab90d202d05
Aurélien <aurelien.catinon@gmail.com>
2021-10-26 11:09:11 +0200

    Move API to .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,13 @@
 import "@nomiclabs/hardhat-ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.alchemyapi.io/v2/7xeGs22pDCMlsMC5ZiasB0XfC41g0F9z",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_KEY}`,
         blockNumber: 13411400
       },
       chainId: 1337
