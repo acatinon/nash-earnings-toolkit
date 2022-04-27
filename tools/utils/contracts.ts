7877f61145d63b41a687d1fe87bb9dec6f612c35
acatinon <aurelien.catinon@gmail.com>
2022-04-27 08:44:12 +0000

    Add Anchor earning contract getter

--- tools/utils/contracts.ts
+++ tools/utils/contracts.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
-import { ContractAddresses, AaveEarningAddress, ABIs } from "./constants.js";
+import { ContractAddresses, AaveEarningAddress, AnchorEarningAddress, ABIs } from "./constants.js";
 import earningAaveAbi from "../ABIs/earningAave.json" assert { type: 'json' };
+import earningAnchorAbi from "../ABIs/earningAnchor.json" assert { type: 'json' };
 
 export function getContractFromToken(token, provider): ethers.Contract {
   return new ethers.Contract(ContractAddresses[token], ABIs[token], provider);
@@ -8,4 +9,8 @@ export function getContractFromToken(token, provider): ethers.Contract {
 
 export function getAaveEarningContract(provider): ethers.Contract {
   return new ethers.Contract(AaveEarningAddress, earningAaveAbi, provider);
+}
+
+export function getAnchorEarningContract(provider): ethers.Contract {
+  return new ethers.Contract(AnchorEarningAddress, earningAnchorAbi, provider);
 }
